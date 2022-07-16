87852d9131929d6e04f8b8b82ff0b40d0fcd93cc
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-07-16 14:21:35 +0200

    Added undo / redo

--- main.js
+++ main.js
@@ -18,5 +18,6 @@ const renderer = new GridRenderer(canvas, 70, 0, generateResult.grid, image);
 
 const game = new Game(renderer, generateResult);
 window.gameState = game.gameState;
+window.renderer.renderer;
 
 renderer.render();
